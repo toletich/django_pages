b8e9bf103ebfdbfd806b0483678f67554a6151bd
Chanakya Toleti <toletibigc@gmail.com>
2022-04-14 11:21:13 -0500

    initial commit

---
+++ pages/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+# pages/tests.py
+from django.test import SimpleTestCase
+
+class PageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
